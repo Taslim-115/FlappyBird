4f508141d98a364433b997278a26b150e2118867
Taslim-115 <taslim.hossain@northsouth.edu>
2023-10-24 19:01:22 +0600

    Changes in update method

--- src/main/java/com/example/cse215lp/Pipe.java
+++ src/main/java/com/example/cse215lp/Pipe.java
@@ -49,13 +49,40 @@ public class Pipe extends Sprite {
         //positionX -= 1; // Adjust the speed
 
         this.setPositionX(getPositionX() - 5);
-        if (getPositionX() - 5 + getWidth() <= 0) this.setPositionX(1000);
+        if (this.getPositionX() - 5 + this.getWidth() <= 0) {
+            this.setPositionX(1000);
+
+        }
+
+
     }
 
     public void update(List<Pipe> pipes) {
         // Update the position of all pipes
+        int randomHeight;
+
+
+        for (int i = 0; i < pipes.size(); i += 2) {
+            // Access the properties or methods of each Pipe object using the index i
+            Pipe pipe = pipes.get(i); // Get the Pipe object at index i
+
+            if(pipe.getPositionX()+ pipe.getWidth() <= 0){
+
+                randomHeight = 250 + (int) (Math.random() * (450 - 250));
+                pipe.setHeight(randomHeight);
+                pipe = pipes.get(i + 1);
+                pipe.setPositionY(randomHeight + 200);
+                pipe.setHeight(800 - (randomHeight + 200));
+            }
+
+
+        }
+
         for (Pipe pipe : pipes) {
             pipe.updateS();
         }
+
+
+
     }
 }
